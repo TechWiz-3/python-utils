3ac3ef9fe5d89b6385370f58a7e873f5a392a0de
TechWiz-3 <programmingplus1@gmail.com>
2022-07-11 14:13:51 +1000

    📦 NEW: currently working -m from get_day import GetDay

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(
+    name='wise-py-utils',
+    version='0.0.1',
+    description='Python utils created by Zac the Wise',
+    package_dir={'': 'datetime-utils'},
+    py_modules=('get_day')
+)
