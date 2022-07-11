803180143b9dcc31b44e175402ab5270c2155f98
TechWiz-3 <programmingplus1@gmail.com>
2022-07-11 14:59:08 +1000

    👌 IMPROVE: modules now working

--- setup.py
+++ setup.py
@@ -5,7 +5,11 @@ setup(
     name='wise-py-utils',
     version='0.0.1',
     description='Python utils created by Zac the Wise',
-    package_dir={'get_day': 'src/datetime-utils',},
+    packages=find_packages(
+        where='src',
+    ),
+    package_dir={"": "src"}
+    #package_dir={'get_day': 'src/datetime-utils',},
     # packages=find_packages(where="src")
     #py_modules=('datetimeutils'),
     #packages=['datetime-utils']
