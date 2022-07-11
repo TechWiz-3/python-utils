f5419813616650898c4a32da995725cf84e511b2
TechWiz-3 <programmingplus1@gmail.com>
2022-07-11 16:39:00 +1000

    👌 IMPROVE: put actual info

--- src/wise/__init__.py
+++ src/wise/__init__.py
@@ -1,6 +1,5 @@
 # Information
 __license__ = 'GPL3'
-__version__ = '1.9.3'
+__version__ = '0.0.1'
 __release__ = False
-__author__ = __maintainer__ = 'Roman Zimbelmann'
-__email__ = 'hut@hut.pm'
+__author__ = __maintainer__ = 'Zac the Wise'
